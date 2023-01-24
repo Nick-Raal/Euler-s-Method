77d969515fe1d7dda7fe7ff9193062524751d638
Nicholas Raal <105031104+Nick-Raal@users.noreply.github.com>
2023-01-24 20:58:10 +0000

    I made it work but I don't actually know if it calculates properly.

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 import java.util.*;
 
 class Main {
+  static List<Double> x = new ArrayList<Double>();
+  static List<Double> y = new ArrayList<Double>();
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    List<Double> x = new ArrayList<Double>();
-    List<Double> y = new ArrayList<Double>();
+
     System.out.println("Step Size?");
     double stepSize = Double.parseDouble(in.nextLine());
     System.out.println("Starting point?");
@@ -12,7 +13,22 @@ class Main {
     toParse = toParse.substring(1, toParse.length() - 1);
     x.add(Double.parseDouble(toParse.substring(0, toParse.indexOf(','))));
     y.add(Double.parseDouble(toParse.substring(toParse.indexOf(',') + 1, toParse.length()))); 
+    System.out.println("End x?");
+    double endPoint = in.nextDouble(); 
+    System.out.println(thing(x.get(0), y.get(0), endPoint, stepSize));
   }
 
-  
+  private static double thing (double X, double Y, double endPoint, double stepSize){
+    x.add(X + stepSize);
+    y.add(y.get(y.size() - 1) + stepSize * equation(x.get(x.size() - 1), y.get(y.size() - 1)));
+
+    if(x.get(x.size() - 1) >= endPoint){
+      return y.get(y.size() - 1);
+    }
+    return thing(x.get(x.size() - 1), y.get(y.size() - 1), endPoint, stepSize);
+  }
+
+  private static double equation(double x, double y){
+    return 2*x + y;
+  }
 }
